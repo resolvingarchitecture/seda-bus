37e7881bd49701a3d579f36a0f2aa92fca43eed1
objectorange <objectorange@1m5.io>
2020-03-04 16:27:05 -0700

    update license

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,36 @@
 
-struct MessageBus {
+mod seda {
+
+    use scheduled_thread_pool::ScheduledThreadPool;
+
+    struct Message {
+
+    }
+
+    struct MessageChannel {
+
+    }
+
+    /// A combination of a common data model, a common command set, and a messaging infrastructure
+    /// to allow different systems to communicate through a shared set of interfaces.
+    struct MessageBus {
+        pool: ScheduledThreadPool
+    }
+
+    impl MessageBus {
+        fn new() -> MessageBus {
+            MessageBus {
+                pool: ScheduledThreadPool::with_name( &name, size)
+            }
+        }
+    }
+
+    /// Messaging Client for connecting to a Message Channel as a Producer and/or Consumer
+    struct MessageEndpoint {
+
+    }
 
 }
-
-struct Channel {
-
-}
-
 #[cfg(test)]
 mod tests {
     #[test]
