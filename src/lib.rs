d14f618d44501f2d6210ed4df4a99ba43c695790
objectorange <objectorange@1m5.io>
2020-03-05 00:58:18 -0700

    remove message and message endpoint; determine number of threads

--- src/lib.rs
+++ src/lib.rs
@@ -3,31 +3,27 @@ mod seda {
 
     use scheduled_thread_pool::ScheduledThreadPool;
 
-    struct Message {
-
-    }
-
     struct MessageChannel {
 
     }
 
     /// A combination of a common data model, a common command set, and a messaging infrastructure
     /// to allow different systems to communicate through a shared set of interfaces.
+    /// This MessageBus takes
     struct MessageBus {
         pool: ScheduledThreadPool
     }
 
     impl MessageBus {
-        fn new(name: &mut String, size: usize) -> MessageBus {
+        fn new(name: &mut String, num_channels: usize) -> MessageBus {
+            let mut num_threads: i8 = (num_channels / 2) as i8;
+            if num_threads % 2 != 0 {
+                num_threads += 1; // add one for dead-letter channel
+            }
             MessageBus {
-                pool: ScheduledThreadPool::with_name( &name, size)
+                pool: ScheduledThreadPool::with_name( &name, num_channels)
             }
         }
-    }
-
-    /// Messaging Client for connecting to a Message Channel as a Producer and/or Consumer
-    struct MessageEndpoint {
-
     }
 
 }
@@ -36,5 +32,9 @@ mod tests {
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+    #[test]
+    fn bus() {
+
     }
 }
