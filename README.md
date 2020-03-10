c8faa5153e9186e73d18e5589c91169061905e8f
objectorange <objectorange@1m5.io>
2020-03-10 01:47:27 -0700

    much implementation; not building as having issue with trait and self

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 # Staged Event-Driven Architecture (SEDA) Bus
 A form of message bus avoiding the high overhead of thread-based concurrency models where channels get their own inbound and outbound queues. 
 The bus uses its own thread pool to route messages between queues. Addresses in Routes point to channel names.
-When used in conjunction with the [Service Bus](https://github.com/resolvingarchitecture/service-bus), the service bus acts as a layer above the message bus driving
-routing based on service operations and/or other logic which results in mappings to the lower message bus address.
+When used in conjunction with the [Service Bus](https://github.com/resolvingarchitecture/service-bus), 
+the service bus acts as a layer above the message bus driving routing based on service operations and/or other 
+logic which results in mappings to the lower message bus address.
 
 [Crates.io](https://crates.io/crates/seda_bus)
 
@@ -11,17 +12,17 @@ routing based on service operations and/or other logic which results in mappings
 
 ## Design Goals 
 
-* to support something akin to a 'simpler decentralized [Kafka](https://engineering.linkedin.com/kafka/benchmarking-apache-kafka-2-million-writes-second-three-cheap-machines)' placing the burden of queue checks on the clients for maximum scalability
-* persist all messages immediately providing configurable retention times
+* place the burden of queue checks on the clients for maximum scalability
+* non-persistence of messages for deniability
 * mark messages as consumed on consumption so that clients can continue on if synchronous
-* mark messages as completed on completion so that clients know when they can clear the log if they wish
+* mark messages as completed on completion so that clients know when they can clear the queue if they wish
 * mark messages as errored with code on error so that clients can determine how they wish to handle it
 * use [dbus](https://en.wikipedia.org/wiki/D-Bus) for inter-process communications on Linux
 * use [ipcd](https://dev.to/legolord208/programming-for-redox-os-4124) for inter-process communications on RedoxOS
 
 ## Functionality
 
-1. Start message bus up with a name, number of channels, and channel prefix. 
-    1. Scheduled Thread Pool is created with name with the number of channels = channels / 2 (+1 if odd number of channels)
-    2. Create a Message Channel for each number of channels and name it prefix_number starting with 1.
-    3. Allocate an Inbound Persistent Queue and Outbound Persistent Queue per Channel.
+1. Start Message Bus up with a name.
+    1. Message bus instance creates a thread pool with Max Capacity of threads.
+2. Create Endpoints as needed using Message Bus.
+   
--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,153 @@
 
+/// A combination of a common data model, a common command set, and a messaging infrastructure
+/// to allow different systems to communicate through a shared set of interfaces.
+/// This MessageBus takes
 mod seda {
 
     use scheduled_thread_pool::ScheduledThreadPool;
+    use std::collections::HashMap;
+    use std::sync::mpsc::{channel, Sender, Receiver, RecvError, SendError};
+    use std::thread;
 
-    struct MessageChannel {
-        name: String
+    use ra_common::{Envelope, Route, LifeCycle, Consumer, Producer, Router};
+
+    const MAXIMUM_CAPACITY: usize = 10;
+
+    /// An address on the network with its inbound and outbound channels.
+    struct MessageEndpoint {
+        _address: String,
+        _in: Box<MessageChannel>,
+        _out: Box<MessageChannel>
     }
 
-    /// A combination of a common data model, a common command set, and a messaging infrastructure
-    /// to allow different systems to communicate through a shared set of interfaces.
-    /// This MessageBus takes
-    struct MessageBus {
-        pool: ScheduledThreadPool
+    impl MessageEndpoint {
+        fn new(address: String, consumer: Box<dyn Consumer>, bus: Box<dyn Consumer>) -> Box<MessageEndpoint> {
+            Box::new(MessageEndpoint {
+                _address: address,
+                _in: MessageChannel::new(consumer),
+                _out: MessageChannel::new(bus)
+            })
+        }
+    }
+
+    impl Producer for MessageEndpoint {
+        fn send(&self, env: Box<Envelope>) -> Result<(), SendError<Box<Envelope>>> {
+            self._out._sender.send(env)
+        }
+    }
+
+    struct MessageChannel {
+        _accepting: bool,
+        _sender: Sender<Box<Envelope>>,
+        _receiver: Receiver<Box<Envelope>>,
+        _consumer: Box<dyn Consumer>
+    }
+
+    impl MessageChannel {
+        fn new(consumer: Box<dyn Consumer>) -> Box<MessageChannel> {
+            let (sender, receiver) = channel();
+            Box::new(MessageChannel {
+                _accepting: false,
+                _sender: sender,
+                _receiver: receiver,
+                _consumer: consumer
+            })
+        }
+    }
+
+    pub struct MessageBus {
+        _pool: ScheduledThreadPool,
+        _endpoints: HashMap<String, Box<MessageEndpoint>>
+    }
+
+    impl LifeCycle for MessageBus {
+        fn start(&self) {
+            unimplemented!()
+        }
+
+        fn restart(&self) {
+            unimplemented!()
+        }
+
+        fn pause(&self) {
+            unimplemented!()
+        }
+
+        fn unpause(&self) {
+            unimplemented!()
+        }
+
+        fn stop(&self) {
+            unimplemented!()
+        }
+
+        fn graceful_stop(&self) {
+            unimplemented!()
+        }
+    }
+
+    impl Router for MessageBus {
+        fn route(&self, env: Box<Envelope>) -> Option<Route> {
+            unimplemented!()
+        }
+    }
+
+    impl Consumer for MessageBus {
+        fn receive(&self, env: Box<Envelope>) {
+            let opt = self.route(env);
+        }
     }
 
     impl MessageBus {
-        fn new(name: &mut String, num_channels: usize) -> MessageBus {
-            let mut num_threads: i8 = (num_channels / 2) as i8;
-            if num_threads % 2 != 0 {
-                num_threads += 1; // add one for dead-letter channel
-            }
-            MessageBus {
-                pool: ScheduledThreadPool::with_name( &name, num_channels)
-            }
+        pub fn new(name: String) -> Box<MessageBus> {
+            let endpoints = HashMap::with_capacity(MAXIMUM_CAPACITY);
+            Box::new(MessageBus {
+                _pool: ScheduledThreadPool::with_name( &name, MAXIMUM_CAPACITY),
+                _endpoints: endpoints
+            })
         }
+        pub fn create_endpoint(&mut self, address: String, consumer: Box<dyn Consumer>) -> &Box<MessageEndpoint> {
+            let mut endpoint = MessageEndpoint::new(address, consumer, Box::new(self));
+            self._endpoints.insert(address, endpoint);
+            return &endpoint;
+        }
+
     }
 
 }
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use crate::seda::MessageBus;
+    use ra_common::{LogConsumer, Envelope, Route, Producer, Consumer};
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
     #[test]
     fn bus() {
+        // Setup bus and endpoints
+        let mut bus = MessageBus::new(format!("{}","test_bus"));
+        let consumer_a = LogConsumer::new();
+        let end_a = bus.create_endpoint(format!("{}","A"), consumer_a);
+        let consumer_b = LogConsumer::new();
+        let end_b = bus.create_endpoint(format!("{}","B"), consumer_b);
+        let consumer_c = LogConsumer::new();
+        let end_c = bus.create_endpoint(format!("{}","C"), consumer_c);
+        let consumer_d = LogConsumer::new();
+        let end_d = bus.create_endpoint(format!("{}","D"), consumer_d);
+        // Send envelopes and check
+        let env = Envelope::new();
+        let r = Route::new(
+            String::from("ServiceA"),
+            String::from("sendToPeer"),
+            String::from("A"),
+            String::from("B"),
+            String::from("A"),
+            String::from("B"));
+        env.slip.add_route(r);
+        end_a.send(env);
 
     }
 }
