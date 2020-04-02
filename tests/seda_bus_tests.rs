2068e0991b21b33930298efbdf80594e6c1b6298
objectorange <brian@resolvingarchitecture.io>
2020-04-01 21:49:15 -0700

    remove crates

--- tests/seda_bus_tests.rs
+++ tests/seda_bus_tests.rs
@@ -3,10 +3,6 @@ extern crate simple_logger;
 
 use log::{trace,info};
 
-use ra_common::models::{Envelope,LifeCycle,Route};
-use seda_bus::bus::MessageBus;
-use seda_bus::endpoint::MessageEndpoint;
-
 #[test]
 fn it_works() {
     assert_eq!(2 + 2, 4);
