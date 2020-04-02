6af8a7d73731ce8de24b4cdf9feecb6ab970513e
objectorange <brian@resolvingarchitecture.io>
2020-04-02 00:29:10 -0700

    registration on router

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -6,58 +6,29 @@ use std::sync::mpsc::{channel, Sender, Receiver, RecvError, RecvTimeoutError};
 use std::collections::HashMap;
 use std::thread;
 use std::time::Duration;
-use seda_bus::{Consumer, MessageChannel, Router, Envelope};
+use seda_bus::{MessageChannel, Router, Envelope};
 
 
 fn main() {
     simple_logger::init().unwrap();
     trace!("Starting SEDA Bus...");
 
-    let mut c_1 = Consumer::new(1);
-    let mut ch_1 = MessageChannel::new(1);
-    let mut rec_1 = ch_1._rx;
-    let mut send_1 = ch_1._tx;
-
-    let mut c_2 = Consumer::new(2);
-    let mut ch_2 = MessageChannel::new(2);
-    let mut rec_2 = ch_2._rx;
-    let mut send_2 = ch_2._tx;
-
-    let mut r = Router::new(send_1, send_2);
+    let mut r = Router::new();
+    r.register(12);
 
     for n in 1..10 {
-        let env = Envelope::new(2, format!("Hello World 2: {}",n));
+        let env = Envelope::new(12, format!("Hello World 12: {}",n));
         r.route(env);
     }
-    for n in 1..10 {
-        let env = Envelope::new(1, format!("Hello World 1: {}",n));
-        r.route(env);
-    }
-    thread::spawn(move || {
+    thread::spawn( move || {
+        let addr :u64 = 12;
         loop {
-            match rec_1.recv_timeout(Duration::from_millis(100)) {
-                Ok(env) => c_1.receive(env),
-                Err(e) => info!("{}","timedout")
+            match r.poll(addr) {
+                Some(env) => info!("env to={} msg={}", env.to, env.msg),
+                None => info!("No msg for addr: {}", addr)
             }
         }
     });
-    thread::spawn(move || {
-        loop {
-            match rec_2.recv_timeout(Duration::from_millis(100)) {
-                Ok(env) => c_2.receive(env),
-                Err(e) => info!("{}","timedout")
-            }
-        }
-    });
-    for n in 10..20 {
-        let env = Envelope::new(1, format!("Hello World 1: {}",n));
-        r.route(env);
-    }
-    thread::sleep(Duration::from_secs(1));
-    for n in 10..20 {
-        let env = Envelope::new(2, format!("Hello World 2: {}",n));
-        r.route(env);
-    }
 
     thread::sleep(Duration::from_secs(1));
     trace!("SED Bus Stopped.");
--- src/lib.rs
+++ src/lib.rs
@@ -1,78 +1,76 @@
 extern crate log;
 
 use log::{trace,info,warn};
-use std::sync::mpsc::{channel, Sender, Receiver, RecvError, RecvTimeoutError};
+use std::sync::mpsc::{channel, Sender, Receiver, RecvError, RecvTimeoutError, SendError};
 use std::collections::HashMap;
 use std::thread;
 use std::time::Duration;
 
+#[derive(Clone)]
 pub struct Envelope {
-    _to_addr: u64,
-    _msg: String
+    pub to: u64,
+    pub msg: String
 }
 
 impl Envelope {
-    pub fn new(to_addr: u64, msg: String) -> Envelope {
-        Envelope {
-            _to_addr: to_addr,
-            _msg: msg
-        }
-    }
-}
-
-pub struct Consumer {
-    _id: u64
-}
-
-impl Consumer {
-    pub fn new(id: u64) -> Consumer {
-        Consumer {
-            _id: id
-        }
-    }
-    pub fn receive(&self, env: Envelope) {
-        info!("Consumer {}: {}",self._id, env._msg);
+    pub fn new(to: u64, msg: String) -> Envelope {
+        Envelope { to, msg }
     }
 }
 
 pub struct MessageChannel {
-    pub _accepting: bool,
-    pub _addr: u64,
-    pub _tx: Sender<Envelope>,
-    pub _rx: Receiver<Envelope>
+    pub accepting: bool,
+    pub addr: u64,
+    pub tx: Sender<Envelope>,
+    pub rx: Receiver<Envelope>
 }
 
 impl MessageChannel {
     pub fn new(addr: u64) -> MessageChannel {
         let (tx, rx) = channel();
-        MessageChannel {
-            _accepting: true,
-            _addr: addr,
-            _tx: tx,
-            _rx: rx
-        }
+        MessageChannel { accepting: true, addr, tx, rx }
     }
 }
 
 pub struct Router {
-    _send_1: Sender<Envelope>,
-    _send_2: Sender<Envelope>
+    channels: HashMap<u64,MessageChannel>
 }
 
 impl Router {
-    pub fn new(send_1: Sender<Envelope>, send_2: Sender<Envelope>) -> Router {
+    pub fn new() -> Router {
         Router {
-            _send_1: send_1,
-            _send_2: send_2
+            channels: HashMap::new()
         }
     }
-    pub fn route(&mut self, env: Envelope) {
-        if env._to_addr == 1 {
-            self._send_1.send(env);
-        } else if env._to_addr == 2 {
-            self._send_2.send(env);
-        } else {
-            warn!("address {} has no channel",env._to_addr);
+    pub fn register(&mut self, addr: u64) {
+        self.channels.insert(addr, MessageChannel::new(addr));
+    }
+    pub fn register_all(&mut self, addr: [u64;2]) {
+        info!("register {:?}",addr);
+        for i in 0..1 {
+            self.channels.insert(addr[i], MessageChannel::new(addr[i]));
+        }
+    }
+    pub fn poll(&mut self, addr: u64) -> Option<Envelope> {
+        match self.channels.get(&addr) {
+            Some(ch) => {
+                match ch.rx.recv_timeout(Duration::from_millis(100)) {
+                    Ok(env) => Option::Some(env),
+                    Err(e) => Option::None
+                }
+            },
+            None => Option::None
+        }
+    }
+    pub fn route(&mut self, env: Envelope) -> bool {
+        match self.channels.get(&env.to) {
+            Some(ch) => {
+                match ch.tx.send(env) {
+                    Ok(()) => true,
+                    Err(e) => false
+                }
+            },
+            None => false
         }
     }
 }
